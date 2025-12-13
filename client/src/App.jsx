acde8ed93ada29c44ab2638afab8801c2e1604b0
CodeWithRajMishra <rajmishra3@gmail.com>
2025-12-13 12:36:55 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import AdminLogin from "./admin/AdminLogin";
+import AdminDashBoard from "./admin/AdminDashBoard";
+import AddProduct from "./admin/AddProduct";
+
+
+const App=()=>{
+  return(
+    <>
+       <BrowserRouter>
+       <Routes>
+        <Route path="/" element={<Layout/>} >
+        <Route index element={<Home/>}/>
+        <Route path="home" element={<Home/>}/>
+        <Route path="admin" element={<AdminLogin/>}/>
+        </Route>   
+        </Routes>    
+
+        <Routes>
+            <Route path="dashboard" element={<AdminDashBoard/>}>
+              <Route path="addproduct" element={<AddProduct/>} />
+            </Route>
+        </Routes>
+       </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const AdminRoute = require("./routes/adminRoute");
+mongoose.connect(process.env.DBCONN).then(()=>{
+    console.log("Database Succesfully Connected!");
+})
+
+// Use body-parser middleware for JSON and URL-encoded data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use("/admin", AdminRoute);
+
+
+
+
+
+
+app.listen(8000, ()=>{
+    console.log("server run on 8000 Port!")
+})
